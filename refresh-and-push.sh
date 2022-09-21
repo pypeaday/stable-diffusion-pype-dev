2522973dc24af48169f1072a7ec93e39d25578d8
Pypeaday <pypeaday@pm.me>
2022-09-21 15:43:51 -0500

    wip: supporting images from AUTOMATIC111 webui

--- refresh-and-push.sh
+++ refresh-and-push.sh
@@ -2,8 +2,12 @@
 
 
 rsync -r /home/nic/personal/stable-diffusion/outputs/img-samples/ static
+
 cp /home/nic/personal/stable-diffusion/outputs/img-samples/dream_log.txt dream_log.txt
 cp /home/nic/personal/stable-diffusion/outputs/img-samples/dream_web_log.txt dream_web_log.txt
+
+# these images come from AUTOMATIC111's webui, the filenames are the prompts
+rsync -r /home/nic/third-party/stable-diffusion-webui/outputs/txt2img-images/ AUTOMATIC111-images
 
 python sqooshem.py
 
