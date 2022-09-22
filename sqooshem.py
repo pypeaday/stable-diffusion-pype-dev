0838a776b35a2038180741d3362173957833d3ce
Pypeaday <pypeaday@pm.me>
2022-09-22 06:56:56 -0500

    update sqooshem

--- sqooshem.py
+++ sqooshem.py
@@ -1,5 +1,10 @@
+import json
 from pathlib import Path
 from subprocess import Popen
+
+with open("source_app_map.json", "r") as f:
+    source_app_map = json.load(f)
+
 
 files = Path("InvokeAI-images").glob("*.png")
 autos_files = Path("AUTOMATIC111-images").glob("*.png")
@@ -7,12 +12,17 @@ autos_files = Path("AUTOMATIC111-images").glob("*.png")
 
 for file in files:
 
+    if file.name in source_app_map.keys():
+        continue
     if (
         Path(str(file).replace("InvokeAI-images", "static"))
         .with_suffix(".webp")
         .exists()
     ):
+        if file.name not in source_app_map.keys():
+            source_app_map[file.name] = "InvokeAI"
         continue
+
     cmd = (
         """npx @squoosh/cli --webp '{"quality":70,"target_size":0,"target_PSNR":0,"method":4,"sns_strength":50,"filter_strength":60,"filter_sharpness":0,"filter_type":1,"partitions":0,"segments":4,"pass":1,"show_compressed":0,"preprocessing":0,"autofilter":0,"partition_limit":0,"alpha_compression":1,"alpha_filtering":1,"alpha_quality":100,"lossless":0,"exact":0,"image_hint":0,"emulate_jpeg_size":0,"thread_level":0,"low_memory":0,"near_lossless":100,"use_delta_palette":0,"use_sharp_yuv":0}' """
         + f"'{str(file)}'"  # need filename in quotes since AUTOMATIC111's images have spaces in name
@@ -21,13 +31,18 @@ for file in files:
     proc = Popen(cmd, shell=True)
     proc.wait()
 
+    source_app_map[file.name] = "InvokeAI"
+
 for file in autos_files:
     if (
         Path(str(file).replace("AUTOMATIC111-images", "static"))
         .with_suffix(".webp")
         .exists()
     ):
+        if file.name not in source_app_map.keys():
+            source_app_map[file.name] = "AUTOMATIC1111"
         continue
+
     cmd = (
         """npx @squoosh/cli --webp '{"quality":70,"target_size":0,"target_PSNR":0,"method":4,"sns_strength":50,"filter_strength":60,"filter_sharpness":0,"filter_type":1,"partitions":0,"segments":4,"pass":1,"show_compressed":0,"preprocessing":0,"autofilter":0,"partition_limit":0,"alpha_compression":1,"alpha_filtering":1,"alpha_quality":100,"lossless":0,"exact":0,"image_hint":0,"emulate_jpeg_size":0,"thread_level":0,"low_memory":0,"near_lossless":100,"use_delta_palette":0,"use_sharp_yuv":0}' """
         + f"'{str(file)}'"  # need filename in quotes since AUTOMATIC111's images have spaces in name
@@ -36,3 +51,9 @@ for file in autos_files:
     )
     proc = Popen(cmd, shell=True)
     proc.wait()
+
+    source_app_map[file.name] = "AUTOMATIC1111"
+
+
+with open("source_app_map.json", "w") as f:
+    json.dump(source_app_map, f)
