07ae5e59a3297303a9b643a3abd36df92a1764f2
Pypeaday <pypeaday@pm.me>
2022-09-22 07:37:11 -0500

    fix comment typo

--- stage-images.sh
+++ stage-images.sh
@@ -5,7 +5,7 @@ rsync -r /home/nic/personal/stable-diffusion/outputs/img-samples/ InvokeAI-image
 cp /home/nic/personal/stable-diffusion/outputs/img-samples/dream_log.txt dream_log.txt
 cp /home/nic/personal/stable-diffusion/outputs/img-samples/dream_web_log.txt dream_web_log.txt
 
-# these images come from AUTOMATIC111's webui, the filenames are the prompts
+# these images come from AUTOMATIC1111's webui, the filenames are the prompts
 rsync -r /home/nic/third-party/stable-diffusion-webui/outputs/txt2img-images/ AUTOMATIC1111-images
 
 python sqooshem.py
