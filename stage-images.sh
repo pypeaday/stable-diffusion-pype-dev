c355a52b4b2d2f8e8073e639b5da2e9fc89c1454
Pypeaday <pypeaday@pm.me>
2022-10-10 09:21:18 -0500

    need to replace spaces with _ in jpg filenames for conversion to png

--- stage-images.sh
+++ stage-images.sh
@@ -9,7 +9,24 @@ cp /home/nic/personal/stable-diffusion/outputs/img-samples/dream_web_log.txt dre
 rsync -r /home/nic/third-party/stable-diffusion-webui/outputs/txt2img-images/ AUTOMATIC1111-images
 rsync -r /home/nic/third-party/stable-diffusion-webui/outputs/txt2img-grids/ AUTOMATIC1111-images
 
+# replace spaces in jpg files with _ for converting to png
+for f in AUTOMATIC1111-images/*.jpg
+do
+  new="${f// /_}"
+  if [ "$new" != "$f" ]
+  then
+    if [ -e "$new" ]
+    then
+      echo not renaming \""$f"\" because \""$new"\" already exists
+    else
+      echo moving "$f" to "$new"
+    mv "$f" "$new"
+  fi
+fi
+done
+
 # convert .jpg to .png for sqooshing
+
 ls -1 AUTOMATIC1111-images/*.jpg | xargs -n 1 bash -c 'convert "$0" "${0%.jpg}.png"'
 
 python sqooshem.py
