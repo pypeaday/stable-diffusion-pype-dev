ec14cc941755a87cdff9d5b8bcdf9e920738babb
Pypeaday <pypeaday@pm.me>
2022-11-04 07:04:52 -0500

    interactive clean working in super basic fashion now for automatic1111's images

--- stable_diffusion_pype_dev/interactive_clean.py
+++ stable_diffusion_pype_dev/interactive_clean.py
@@ -12,46 +12,37 @@ with open("source_app_map_golden.json", "r") as f:
     source_app_map_golden = json.load(f)
 
 
-pics_root = "/home/nic/third-party/stable-diffusion-webui/outputs/"
+automatic1111_pics_root = "/home/nic/third-party/stable-diffusion-webui/outputs/"
+invokeai_pics_root = "/home/nic/personal/stable-diffusion/outputs/"
+
+
+AUTOMATIC1111_IMAGES = [im for im in Path(automatic1111_pics_root).glob("**/*.png")]
+INVOKEAI_IMAGES = [im for im in Path(invokeai_pics_root).glob("**/*.png")]
 
 
 @dataclass
 class Image:
 
-    name: str
+    file: Path
     source: str
 
     def __post_init__(self):
         # stupidedly glob over all the files to find this one
         # necessary for automatic1111 cause there's 4 possible directories between outputs and the actual image
-        for _pic in Path(pics_root).glob("**/*.png"):
-            if _pic.name == self.name:
-                self.source_png_path = _pic
-                break
-        if self.source == "AUTOMATIC1111":
-            self.repo_png_path = Path("AUTOMATIC1111-images", self.name)
-        elif self.source == "InvokeAI":
-            raise NotImplementedError
-        self.repo_webp_path = Path("static", Path(self.name).with_suffix(".webp"))
+
+        self.repo_webp_path = Path("static", Path(self.file.name).with_suffix(".webp"))
 
     def delete(self):
-        # delete the file
-        if self.repo_png_path.exists():
-            # if there's no image then unlink won't work. This might happen if an image gets deleted but the entry didn't get removed from source_app_map somehow
-            self.repo_png_path.unlink()
-        else:
-            pass
-            # breakpoint()
-        if self.source_png_path.exists():
+        if self.file.exists():
             # also make sure to try to remove the png in this repo
-            self.source_png_path.unlink()
+            self.file.unlink()
         if self.repo_webp_path.exists():
             # also make sure to try to remove the web in static
             self.repo_webp_path.unlink()
         else:
             pass
         # remove it from source_app_map
-        source_app_map.pop(self.name)
+        source_app_map.pop(str(self.file))
         # re-create the file
         with open("source_app_map.json", "w") as f:
             json.dump(source_app_map, f)
@@ -60,7 +51,7 @@ class Image:
 def serialize_image_containers():
     try:
         images = pickle.load(open("image_cleaning.pkl", "rb"))
-        return [x for x in images if x.name not in source_app_map_golden.keys()]
+        return [x for x in images if str(x.file) not in source_app_map_golden.keys()]
 
     except:
         print("failed loading pickle")
@@ -71,16 +62,17 @@ def serialize_image_containers():
             if pic in source_app_map_golden.keys():
                 print(f"Already keeping {pic} in golden list")
                 continue
-            # create data structure for ease
-            elif source == "AUTOMATIC1111":
-                # source as variable in case I can actually use this dataclass easily for both automatic1111 and invokeAI
-                images.append(Image(pic, source))
-                ...
-            elif source == "InvokeAI":
-                # for InvokeAI repo images
-                ...
-            else:
-                breakpoint()
+            # # create data structure for ease
+            # elif source == "AUTOMATIC1111":
+            #     # source as variable in case I can actually use this dataclass easily for both automatic1111 and invokeAI
+            #     images.append(Image(pic, source))
+            #     ...
+            # elif source == "InvokeAI":
+            #     # for InvokeAI repo images
+            #     ...
+            # else:
+            #     breakpoint()
+            images.append(Image(Path(pic), source))
 
         pickle.dump(images, open("image_cleaning.pkl", "wb"))
         return images
@@ -90,21 +82,23 @@ if __name__ == "__main__":
     images = serialize_image_containers()
     for image in images:
         try:
-            image.path
+            image.file
         except AttributeError:
-            # if image.path doesn't exist then something is wrong with the SOURCE image not being there
-            image.delete()
+            # if image.source_png_path doesn't exist then something is wrong with the SOURCE image not being there
+            print(f"would delete {image.file.name}")
+            # image.delete()
             continue
         # open the image
-        if not Path(image.path).exists():
+        if not Path(image.file).exists():
+            print(f"{image.file} source png does not exist")
             # need to re-pickle otherwise next time we'll get FileNotFoundErrors
             pickle.dump(
-                [x for x in images if x.name != image.name],
+                [x for x in images if x.file.name != image.file.name],
                 open("image_cleaning.pkl", "wb"),
             )
             continue
 
-        with PillowImage.open(image.path) as im:
+        with PillowImage.open(image.file) as im:
             im.show()
             keep = input("Keep? ")
             if "n" in keep.lower():
@@ -113,16 +107,17 @@ if __name__ == "__main__":
                 image.delete()
 
             elif "y" in keep.lower():
-                source_app_map_golden[image.name] = image.source
+                source_app_map_golden[str(image.file)] = image.source
                 with open("source_app_map_golden.json", "w") as f:
                     json.dump(source_app_map_golden, f)
                 # need to re-pickle otherwise next time we'll just load up images we already decided to keep
+                print("remaking pickle")
                 pickle.dump(
-                    [x for x in images if x.name != image.name],
+                    [x for x in images if str(x.file) != str(image.file)],
                     open("image_cleaning.pkl", "wb"),
                 )
             else:
-                print(f"No decision made - passing on {image.name}")
+                print(f"No decision made - passing on {image.file.name}")
         im.close()
 
     # # Trying to delete webp images that made it into static but aren't either tracked in any prompt OR especially if they aren't in golden repo
