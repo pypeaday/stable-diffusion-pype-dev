c1efa0158a17b22604f9f72712d0e3400e73c1ca
Pypeaday <pypeaday@pm.me>
2022-11-04 05:59:41 -0500

    call sqooshem from refresh-and-push

--- refresh-and-push.sh
+++ refresh-and-push.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-./stage-images.sh
+# ./stage-images.sh
 
-git add ./dream_log.txt
-git add ./dream_web_log.txt
+# git add ./dream_log.txt
+# git add ./dream_web_log.txt
+python ./sqooshem.py
 
 git add ./static
 
