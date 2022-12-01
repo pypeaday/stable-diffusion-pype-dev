4cf8e499e1f60315fa8543d08bd19b752e6e0456
Pypeaday <pypeaday@pm.me>
2022-12-01 09:18:17 -0600

    remove cruft from load - only caring about automatic1111's webui

--- stable_diffusion_pype_dev/load.py
+++ stable_diffusion_pype_dev/load.py
@@ -10,52 +10,17 @@ from .models import AUTOMATIC1111WebPrompt
 # source env variables
 load_dotenv()
 
-# from .models import WebPrompt, Prompt
-
-with open("source_app_map.json", "r") as f:
-    source_app_map = json.load(f)
-
 
 @hook_impl
 def configure(markata) -> None:
     markata.content_directories = [Path("static")]
 
 
-def _turn_original_png_path_to_static_webp_path(filepath: str):
-
-    return Path("static", Path(Path(filepath).name)).with_suffix(".webp")
-
-
 @hook_impl
 @register_attr("articles")
 def load(markata) -> None:
-    # articles should be type: List[Union[AUTOMATIC1111WebPrompt, Prompt, WebPrompt]]
-    markata.articles = []
-    # prompts = Path("dream_log.txt").read_text().split("\n")
-    # markata.articles += [
-    #     Prompt(_turn_original_png_path_to_static_webp_path(pair[0]), pair[1])
-    #     for p in prompts
-    #     if len(pair := p.split(":")) == 2
-    # ]
+    markata.articles: List[AUTOMATIC1111WebPrompt] = []
 
-    # prompts = Path("dream_web_log.txt").read_text().split("\n")
-    # web_based_articles: List[WebPrompt] = []
-    # for p in prompts:
-    #     if len(pair := p.split(":")) == 16:
-    #         file, raw_data = (
-    #             _turn_original_png_path_to_static_webp_path(pair[0]),
-    #             pair[1:],
-    #         )
-    #         data = json.loads(":".join(raw_data))
-    #         web_based_articles.append(WebPrompt(file, data))
+    converted_images = list(Path("./static").glob("*.webp"))
 
-    automatic1111_data = (
-        # regardless of which suboutput folder the source images are in, I store them flat in static - so just check for the webp version of the picture in this repo
-        Path("static", Path(Path(k).name).with_suffix(".webp"))
-        for k, v in source_app_map.items()
-        if v == "AUTOMATIC1111"
-    )
-    automatic1111_articles = [AUTOMATIC1111WebPrompt(f) for f in automatic1111_data]
-
-    # markata.articles += web_based_articles
-    markata.articles += automatic1111_articles
+    markata.articles = [AUTOMATIC1111WebPrompt(f) for f in converted_images]
